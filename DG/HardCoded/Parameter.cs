df3d6b73fd7aa692ed5448a087db921253446bb7
awoody <awoody@protonmail.com>
2020-04-24 15:49:46 +0930

    Added more app level parameters.

--- DG/HardCoded/Parameter.cs
+++ DG/HardCoded/Parameter.cs
@@ -4,23 +4,29 @@ namespace DG
 {
    public class Parameter
    {
-
- 
+      
       public List<ParameterTuple> ListOfParameterStrings { get;  set; }
 
       public Parameter()
       {
          ListOfParameterStrings = new List<ParameterTuple>();
   
-         ParameterTuple.AddNewParameterString(ListOfParameterStrings, "filePathAndName", (string)@"C:\git\DataGenerator\GeneratorOutputDefinitions\Presenter.json", "Represents the file to be processed today.");
+         //Issue here will be expanding to multiple Source JSON and multiple MockData Files.
+         ParameterTuple.AddNewParameterString(ListOfParameterStrings, "OutputDefinitionsPathAndName", (string)@"C:\git\DataGenerator\GeneratorOutputDefinitions\Presenter.json", "Represents the file to be processed today.");
+         //ParameterTuple.AddNewParameterString(ListOfParameterStrings, "MockDataPath", parameterValue:(string)@"C:\git\DataGenerator\MockData\", ".");
+
+         ParameterTuple.AddNewParameterString(ListOfParameterStrings, "MockData123YN", parameterValue: (string)@"C:\git\DataGenerator\MockData\123YN.txt", ".");
+         ParameterTuple.AddNewParameterString(ListOfParameterStrings, "Surname", parameterValue: (string)@"C:\git\DataGenerator\MockData\Surname.txt", ".");
+         ParameterTuple.AddNewParameterString(ListOfParameterStrings, "1234567890", parameterValue: (string)@"C:\git\DataGenerator\MockData\1234567890.txt", ".");
+         
 
          ParameterTuple.AddNewParameterString(ListOfParameterStrings, "DefaultInteger", (int)-99, "");
-
          ParameterTuple.AddNewParameterString(ListOfParameterStrings, "DefaultString", (string)@"", "Represents the file to be processed today.");
+         //Set low for initial tetsting.
+         //ParameterTuple.AddNewParameterString(ListOfParameterStrings, "batchProcessing", (int)5, "");
 
       }
-
-
+      
       public static dynamic GetParameterValue(Parameter p, string parameterName)
       {
          ParameterTuple identified = p.ListOfParameterStrings.Find(x => x.ParameterName.Contains(parameterName.ToLower()));
@@ -36,7 +42,6 @@ namespace DG
          }
 
       }
-
 
    }
 }
