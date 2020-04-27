746037c75eb98ed2697e19e1b45e79c17581c2d8
awoody <awoody@protonmail.com>
2020-04-27 14:14:07 +0930

    incrementing numbers updated with example method

---
+++ VSFiles/DG/Incremental/CreateIncrementingNumberList.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace DG
+{
+   public class CreateIncrementingNumberList
+    {
+
+      //    CreateIncrementingNumberList idt = new CreateIncrementingNumberList();
+      //    List<dynamic> numbersIncremental = new List<dynamic>();
+      //    datesIncremental = idt.GenerateIncrementingNumberList(
+      //                                                          AppConst.DefaultNumberRanges.StartNumber,
+      //                                                          AppConst.DefaultNumberRanges.EndNumber
+      //                                                         );
+
+
+      public List<dynamic> GenerateIncrementingNumberList(int start, int end)
+        {
+
+            List <dynamic> numbersIncremental = new List<dynamic>();
+
+            for (int i = start; i < end; i++)
+            {
+               numbersIncremental.Add(i);
+            }
+
+            return numbersIncremental;
+        }
+        
+    }
+}
