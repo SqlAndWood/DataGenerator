3c754e85a0517d7c10154e906a751993555ffc38
awoody <awoody@protonmail.com>
2020-04-30 08:34:49 +0930

    Created class to wrap delimiter around a string.

---
+++ VSFiles/DG/BusinessRules/Quoter.cs
@@ -0,0 +1,27 @@
+﻿namespace DG
+{
+   static class Quoted
+   {
+
+      /*
+       *text = text.SurroundWithDoubleQuotes(); 
+       */
+      public static string SurroundWithDoubleQuotes(this string text)
+      {
+         return SurroundWith(text, "\"");
+         //  return  string.Format("\"{0}\"", text);
+      }
+      /*
+       *text = text.SurroundWith("'"); 
+       */
+      public static string SurroundWith(this string text, string ends)
+      {
+         return ends + text + ends;
+        
+      }
+
+
+
+
+   }
+}
